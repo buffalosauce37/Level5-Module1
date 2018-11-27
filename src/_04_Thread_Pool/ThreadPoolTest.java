b087b568d54e1e0bd39407127e40cf5b017bb442
ryan.palmer <ryan.palmer@servicenow.com>
2018-11-26 19:37:34 -0800

    Reduced value of 'total' by a factor of 2 because previous value can exceed maximum heap size.

--- src/_04_Thread_Pool/ThreadPoolTest.java
+++ src/_04_Thread_Pool/ThreadPoolTest.java
@@ -20,7 +20,7 @@ public class ThreadPoolTest {
 
 	@Test
 	public void test() {
-		int total = 1073741824;
+		int total = 536870912;
 		
 		ThreadPool tp = new ThreadPool(4);
 		char[] chars = new char[total];
